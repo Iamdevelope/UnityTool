1be271805b1e91eabca992f33de75341aa8a98a3
Iamdevelope <986461143@qq.com>
2018-12-25 10:51:58 +0800

    在新建脚本时，在脚本的最上方添加项目信息

---
+++ Scripts/Editor/ChinarScriptFirstComment.cs
@@ -0,0 +1,25 @@
+﻿
+using System.IO;
+using UnityEngine;
+
+public class ChinarScriptFirstComment : UnityEditor.AssetModificationProcessor {
+    /// <summary>
+    /// 在资源创建时调用
+    /// </summary>
+    /// <param name="path">自动传入资源路径</param>
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (!path.EndsWith(".cs")) return;
+        string allText = "// ========================================================\r\n"
+                         + "// Describe：\r\n"
+                         + "// Autho：慕飞 \r\n"
+                         + "// Time：#CreateTime#\r\n"
+                         + "// UnityVersion：" + Application.unityVersion+ "\r\n"
+                         + "// Version：" + Application.version + "\r\n"
+                          + "// ========================================================\r\n";
+        allText += File.ReadAllText(path);
+        allText = allText.Replace("#CreateTime#", System.DateTime.Now.ToString("yyyy-MM-dd"));
+        File.WriteAllText(path, allText);
+    }
+}
